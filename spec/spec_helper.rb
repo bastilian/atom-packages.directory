1ec584643a0f30695cd8ae90fb24f0b5bddceb22
Sebastian Gräßl <sebastian@validcode.me>
2016-04-10 13:46:00 +0200

    Configure rspec with facory girl, faker and database handling

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,24 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..'))
 
 ENV['RETHINKDB_URL'] = 'rethinkdb://database/atom_test'
-
 require 'lib/environment'
-NoBrainer.sync_indexes
+require 'factory_girl'
+require 'faker'
 
-RSpec.configure do |c|
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    NoBrainer.sync_indexes
+  end
+
+  config.before(:all) do
+    NoBrainer.drop!
+    NoBrainer.sync_schema
+    FactoryGirl.find_definitions
+  end
+
+  config.before(:each) do
+    NoBrainer.purge!
+  end
 end
