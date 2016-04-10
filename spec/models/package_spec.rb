af5c3257ca9c088f893eae39c548a58c1900e14e
Sebastian Gräßl <sebastian@validcode.me>
2016-04-10 13:47:18 +0200

    Add specs for Package categorise and uncategorise

--- spec/models/package_spec.rb
+++ spec/models/package_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
-require 'models/package'
 
-describe Package do
-  subject { Package.create(name: 'test') }
+describe Package, type: :model do
+  subject { FactoryGirl.create(:package) }
+  let(:category) { FactoryGirl.create(:category) }
 
-  it "exists" do
-    puts subject.inspect
-    expect(subject.class).to eq(Package)
+  describe '#categorise' do
+    it 'adds a category to categories' do
+      expect do
+        subject.categorise(category)
+      end.to change(PackageCategorisation, :count).by(1)
+    end
+  end
+
+  describe '#uncategorise' do
+    before do
+      subject.categorise(category)
+    end
+
+    it 'removes a category from categories' do
+      expect do
+        subject.uncategorise(category)
+      end.to change(PackageCategorisation, :count).by(-1)
+    end
   end
 end
