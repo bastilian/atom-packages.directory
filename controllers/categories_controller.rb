629369aaea549bf974f12921ab06d6eda58f2c96
Sebastian Gräßl <sebastian@validcode.me>
2016-04-11 21:49:38 +0200

    Remove write operations from legacy controllers

--- controllers/categories_controller.rb
+++ controllers/categories_controller.rb
@@ -4,19 +4,4 @@ class CategoriesController < ApplicationController
 
     erb :category, locals: { category: category }
   end
-
-  post '/category' do
-    category = Category.where(name: params[:name]).first_or_create(description: params[:description])
-
-    erb :category, locals: { category: category }
-  end
-
-  get '/subcategorise/:category/as/:parent_category' do
-    category = Category.where(permalink: params[:category]).first
-    category.update(
-      parent_category: Category.where(permalink: params[:parent_category]).first
-    )
-
-    redirect to("/category/#{category.permalink}")
-  end
 end
