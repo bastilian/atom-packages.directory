852e0a23cca2fac1bd4396195cf8a4f25d2b3610
Sebastian Gräßl <sebastian@validcode.me>
2016-04-13 23:36:19 +0200

    Move tempaltes

--- controllers/categories_controller.rb
+++
@@ -1,9 +0,0 @@
-require 'controllers/application_controller'
-
-class CategoriesController < ApplicationController
-  get '/category/:category' do
-    category = Category.where(permalink: params[:category]).first
-
-    erb :category, locals: { category: category }
-  end
-end
--- controllers/packages_controller.rb
+++
@@ -1,9 +0,0 @@
-require 'controllers/application_controller'
-
-class PackagesController < ApplicationController
-  get '/package/:permalink' do
-    package = PackageDecorator.new(Package.where(permalink: params[:permalink]).first)
-
-    erb :package, locals: { package: package }
-  end
-end
