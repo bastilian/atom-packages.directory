6700225d0c336bc68843fba119257397f5fea8d7
Sebastian Gräßl <sebastian@validcode.me>
2016-04-18 11:54:52 +0200

    Refactor Packages::Categorizer

--- lib/packages_categorizer.rb
+++ lib/packages_categorizer.rb
@@ -1,10 +1,7 @@
 module Packages
   # Takes packages in batches and categorises them based on their keywords
   class Categorizer
-    include Permalink
-    attr_accessor :options,
-                  :categorised,
-                  :uncategorised
+    attr_accessor :options
 
     DEFAULT_OPTIONS = {
       batch_size: 100
@@ -12,47 +9,12 @@ module Packages
 
     def initialize(options = {})
       self.options = DEFAULT_OPTIONS.merge(options)
-      self.categorised = 0
-      self.uncategorised = 0
+      self.class.categorised = 0
+      self.class.uncategorised = 0
     end
 
     def package_count
       @package_count ||= Package.all.count
-    end
-
-    def extract_keywords(package)
-      return unless package.keywords
-      package.keywords.compact.reject(&:empty?).map do |keyword|
-        puts keyword
-        category_name = keyword.capitalize
-        permalink = permalink_from(keyword)
-
-        Category.where(permalink: permalink).first_or_create(name: category_name)
-      end
-    end
-
-    def get_language(package)
-      language_name = package.permalink.gsub(/^language-/, '')
-      language = Category.where(permalink: language_name).first_or_create(name: language_name.tr('-', ' ').capitalize)
-      language.save!
-
-      language
-    end
-
-    def categorise_package(package)
-      categories = [*extract_keywords(package)]
-      categories << get_language(package) if package.permalink =~ /^language-/
-
-      if categories
-        categories.compact.uniq.each do |category|
-          puts "Categorising #{package.name} as #{category.name}"
-          package.categorise(category)
-        end
-        self.categorised += 1
-      else
-        puts "Unable to categorise #{package.name}"
-        self.uncategorised += 1
-      end
     end
 
     def categorise(batch: 1)
@@ -68,7 +30,55 @@ module Packages
     def start
       categorise
       # Cleanup: Remove categories only having one package
-      puts "Categorised #{self.categorised} packages. (Uncategorised: #{self.uncategorised})"
+      puts "Categorised #{self.class.categorised} packages. (Uncategorised: #{self.class.uncategorised})"
+    end
+
+    class << self
+      include Permalink
+      attr_accessor :uncategorised,
+                    :categorised
+
+      def extract_keywords(package)
+        return unless package.keywords
+        package.keywords.compact.reject(&:empty?).map do |keyword|
+          category_name = keyword.capitalize
+          permalink = permalink_from(keyword)
+
+          Category.where(permalink: permalink).first_or_create(name: category_name)
+        end
+      end
+
+      def get_language(package)
+        return unless package.permalink =~ /^language-/
+        language_name = package.permalink.gsub(/^language-/, '')
+        language = Category.where(permalink: language_name).first_or_create(name: language_name.tr('-', ' ').capitalize)
+        language.save!
+
+        language
+      end
+
+      def get_categories(package)
+        categories = [*extract_keywords(package)]
+        categories << get_language(package)
+
+        categories.compact.uniq
+      end
+
+      def categorise_package(package)
+        categories = get_categories(package)
+
+        if !categories.empty?
+          categories.each do |category|
+            puts "Categorising #{package.name} as #{category.name}"
+            package.categorise(category)
+          end
+
+          self.categorised += 1
+        else
+          puts "Unable to categorise #{package.name}"
+          self.uncategorised += 1
+        end
+      end
     end
   end
 end
