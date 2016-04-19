f54a3cd3dca0f6cf35d825e42ac2f104202158b8
Sebastian Gräßl <sebastian@validcode.me>
2016-04-19 11:00:08 +0200

    Add specs for Packages::Categorizer#categorise

--- spec/lib/packages_categorizer_spec.rb
+++ spec/lib/packages_categorizer_spec.rb
@@ -9,34 +9,50 @@ describe Packages::Categorizer do
     package.save
   end
 
-  describe "#package_count" do
-    it "returns the count of packages" do
+  describe '#package_count' do
+    it 'returns the count of packages' do
       expect(subject.package_count).to eq(1)
     end
   end
 
-  describe ".extract_keywords" do
-    it "returns an array of categories" do
+  describe '#categorise' do
+    context 'when no batch_size is set' do
+      it 'retrives packages with a limit calculated from the default' do
+        expect(Package).to receive(:limit).with(100).and_call_original
+        subject.categorise
+      end
+    end
+
+    context 'when a batch is given' do
+      it 'skips batch_size times the batch' do
+        expect_any_instance_of(NoBrainer::Criteria).to receive(:skip).with(100).and_call_original
+        subject.categorise(batch: 2)
+      end
+    end
+  end
+
+  describe '.extract_keywords' do
+    it 'returns an array of categories' do
       expect(Packages::Categorizer.extract_keywords(package).sample.class).to eq(Category)
     end
   end
 
-  describe ".get_language" do
+  describe '.get_language' do
     context "the package is named with 'language-LANG'" do
-      let(:package) { FactoryGirl.create(:package, name: 'language-LANG')}
+      let(:package) { FactoryGirl.create(:package, name: 'language-LANG') }
 
       it 'returns a category for the language' do
         expect(Packages::Categorizer.get_language(package).name).to eq('Lang')
       end
     end
 
-    context "the package is not named as a language" do
+    context 'the package is not named as a language' do
       it 'returns nil' do
         expect(Packages::Categorizer.get_language(package)).to eq(nil)
       end
     end
 
-    describe ".categorise_package" do
+    describe '.categorise_package' do
       it 'adds a categories for a package' do
         expect do
           Packages::Categorizer.categorise_package(package)
