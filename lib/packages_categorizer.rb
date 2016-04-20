a2f1a339c8e03e5df336eb9bf2318862bfa81a22
Sebastian Gräßl <sebastian@validcode.me>
2016-04-20 08:52:29 +0200

    Remove caching for now

--- lib/packages_categorizer.rb
+++ lib/packages_categorizer.rb
@@ -16,7 +16,6 @@ module Packages
       self.config = YAML.load_file(config_file)
       self.class.categorised = 0
       self.class.uncategorised = 0
-      self.class.cached_categories = {}
 
       build_category_tree
     end
@@ -26,7 +25,6 @@ module Packages
         name = category[0].dup
         sub_categories = category[1]
         new_category = Category.create(name: name, parent_category: parent_category)
-        self.class.cached_categories[new_category.permalink] = new_category
 
         if sub_categories
           build_category_tree(categories: category[1], parent_category: new_category)
@@ -57,38 +55,33 @@ module Packages
     class << self
       include Permalink
       attr_accessor :uncategorised,
-                    :categorised,
-                    :cached_categories
+                    :categorised
 
       def extract_keywords(package)
         return unless package.keywords
         package.keywords.compact.reject(&:empty?).map do |keyword|
           category_name = keyword.capitalize
           permalink = permalink_from(keyword)
-          cached_category(permalink: permalink, category_name: category_name)
+          category(permalink: permalink, category_name: category_name)
         end
       end
 
-      def cached_category(permalink:, category_name: nil)
-        return cached_categories[permalink].reload if cached_categories[permalink]
-        puts "Caching: #{permalink}"
+      def category(permalink:, category_name: nil)
         criteria = Category.where(permalink: permalink)
         if category_name
-          cached_categories[permalink] = criteria.first_or_create(name: category_name)
+          criteria.first_or_create(name: category_name)
         else
-          cached_categories[permalink] = criteria.first
+          criteria.first
         end
-
-        cached_categories[permalink]
       end
 
       def get_language(package)
         return unless package.permalink =~ /^language-/
         language_name = package.permalink.gsub(/^language-/, '')
-        language = cached_category(permalink: language_name, category_name: language_name.tr('-', ' ').capitalize)
+        language = category(permalink: language_name, category_name: language_name.tr('-', ' ').capitalize)
 
         unless language.parent_category
-          language.parent_category = cached_category(permalink: 'languages')
+          language.parent_category = category(permalink: 'languages')
           language.save
         end
 
@@ -97,7 +90,7 @@ module Packages
 
       def extract_categories_from_name(package)
         package.name.split('-').map do |name_part|
-          cached_category(permalink: permalink_from(name_part))
+          category(permalink: permalink_from(name_part))
         end
       end
 
