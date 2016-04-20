63926bf1d5c092451cf07e09a89700bf38c04b8f
Sebastian Gräßl <sebastian@validcode.me>
2016-04-20 15:08:28 +0200

    Path is now simpler

--- decorators/category_decorator.rb
+++ decorators/category_decorator.rb
@@ -1,8 +1,5 @@
 class CategoryDecorator < Decorator
   def image_path
-    path = 'category/'
-    path += "#{parent_category.permalink}-" if parent_category
-
-    path + "#{permalink}.png"
+    "category/#{permalink}.png"
   end
 end
