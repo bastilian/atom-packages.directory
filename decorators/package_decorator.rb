a2ec9cd378b4205ce7d0b74afa4d98e67c31a4cb
Sebastian Gräßl <sebastian@validcode.me>
2016-06-01 08:03:32 +0200

    Use non-destructive methods to humanize package name (fixes #14)

--- decorators/package_decorator.rb
+++ decorators/package_decorator.rb
@@ -21,9 +21,9 @@ class PackageDecorator < Decorator
 
   def humanized_name
     string = name
-    string.gsub!(/^(language|atom)-/, '')
-    string.gsub!(/-ui$/, '')
-    string.tr!('-', ' ')
+    string.gsub(/^(language|atom)-/, '')
+    string.gsub(/-ui$/, '')
+    string.tr('-', ' ')
     string.split(/(\W)/).map(&:capitalize).join
   end
 
